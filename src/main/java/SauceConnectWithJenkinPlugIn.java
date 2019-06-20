e104b589afa6f1fad524152458e06aec489256a2
cproint <cproint@gmail.com>
2019-06-19 17:45:49 -0700

    initial commit

---
+++ src/main/java/SauceConnectWithJenkinPlugIn.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+
+
+public class SauceConnectWithJenkinPlugIn {
+
+    static RemoteWebDriver driver;
+
+
+    public static final String USERNAME = System.getenv("SAUCE_USERNAME");
+    public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
+
+    static String URL;
+
+
+
+    public static void main(final String[] args) throws InterruptedException, MalformedURLException {
+
+        RemoteWebDriver driver;
+
+        URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com/wd/hub";
+
+        ChromeOptions caps = new ChromeOptions();
+        caps.setCapability("version", "latest");
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("name", "groupmdemo");
+
+
+        driver = new RemoteWebDriver(new java.net.URL(URL), caps);
+        driver.get("https://www.groupm.com/");
+
+        driver.quit();
+    }
+}
+
